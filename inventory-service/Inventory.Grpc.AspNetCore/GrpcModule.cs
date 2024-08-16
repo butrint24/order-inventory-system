2eb6e827d261e173e70978cd40e4864a11ae306c
Butrint <butrint.salihu24@gmail.com>
2024-08-16 19:33:48 +0200

    feat: added grpc to product service

---
+++ inventory-service/Inventory.Grpc.AspNetCore/GrpcModule.cs
@@ -0,0 +1,22 @@
+﻿using Inventory.Grpc.Contracts;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Inventory.Grpc.AspNetCore;
+
+public static class GrpcModule
+{
+    public static void AddGrpcModule(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddGrpc();
+
+        var productServiceUrl = configuration["Grpc:ProductServiceUrl"];
+
+        services.AddGrpcClient<ProductService.ProductServiceClient>(options =>
+        {
+            options.Address = new Uri(productServiceUrl);
+        });
+
+        services.AddScoped<IProductService, ProductServiceImplement>();
+    }
+}
---
+++ inventory-service/Inventory.Grpc.AspNetCore/ProductServiceImplement.cs
@@ -0,0 +1,44 @@
+﻿using Inventory.Data.Contracts;
+using Inventory.Grpc.Contracts;
+using Product = Inventory.Grpc.Contracts.Product;
+
+namespace Inventory.Grpc.AspNetCore;
+
+public class ProductServiceImplement : IProductService
+{
+    private readonly IDataRepository _dataRepository;
+
+    public ProductServiceImplement(IDataRepository dataRepository) => _dataRepository = dataRepository;
+
+    public async Task<GetProductResponse> GetProduct(GetProductRequest request)
+    {
+        if (string.IsNullOrEmpty(request.ProductId))
+        {
+            throw new ArgumentException("Product ID cannot be empty");
+        }
+
+        if (!Guid.TryParse(request.ProductId, out var productId))
+        {
+            throw new ArgumentException("Invalid Product ID format");
+        }
+
+        var product = await _dataRepository.GetProductById(productId);
+
+        if (product == null)
+        {
+            throw new KeyNotFoundException($"Product with ID {request.ProductId} not found");
+        }
+
+        return new GetProductResponse
+        {
+            Product = new Product
+            {
+                ProductId = product.ProductId.ToString(),
+                Name = product.Name,
+                Price = (double)product.Price,
+                Stock = product.Stock,
+                Details = product.Details ?? string.Empty
+            }
+        };
+    }
+}
