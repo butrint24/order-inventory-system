9b4184e20c9db17c7a914dbe9efb54eb6f99eb5e
Butrint <butrint.salihu24@gmail.com>
2024-08-20 22:15:13 +0200

    fix: project references

--- order-service/Order.Messaging.RabbitMQ/RabbitMqModule.cs
+++ order-service/Order.Messaging.RabbitMQ/RabbitMqModule.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Order.Messaging.Contracts;
 
 namespace Order.Messaging.RabbitMQ;
